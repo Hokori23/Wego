33b9bc0b622a90cf7a0cd4c2c88cac844c285fea
hokori <hokorixiao@wezhuiyi.com>
2020-08-26 17:43:54 +0800

    Feat: 路由配置

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Home } from "./views";
-import "./App.css";
-
+import { AdminPage, Home } from "./views";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.scss";
+const supportsHistory = "pushState" in window.history;
 function App() {
 	return (
-		<div className="App">
-			{/* <header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header> */}
-			<Home />
-		</div>
+		<BrowserRouter forceRefresh={!supportsHistory}>
+			<div className="App">
+				<Route path="/" exact component={Home} />
+				<Route path="/admin" component={AdminPage} />
+			</div>
+		</BrowserRouter>
 	);
 }
 
