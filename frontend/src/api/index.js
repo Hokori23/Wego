cb95de49cfccd4fd58a29c3c3f6e8d2ebf99e3b5
hokori <hokorixiao@wezhuiyi.com>
2020-08-26 17:45:22 +0800

    Feat: 接口配置列表

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,64 @@
-import axios from 'axios'
-axios.defaults.baseURL = 'https://api.hokori.online/wego'
+import axios from "axios";
+axios.defaults.baseURL = "https://api.hokori.online/wego";
 export default {
-    getApplicationList: () => {
-        return axios.get('/get-applicationlist');
-    }
-}
+	/**
+	 * 获取申请列表接口
+	 */
+	getApplicationList: () => {
+		return new Promise((resolve, reject) => {
+			axios
+				.get("/get-applicationlist")
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((e) => {
+					reject(e);
+				});
+		});
+	},
+	/**
+	 * 处理申请接口
+	 */
+	handleApplication: (payload) => {
+		return new Promise((resolve, reject) => {
+			axios
+				.post("/handle", payload)
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((e) => {
+					reject(e);
+				});
+		});
+	},
+	/**
+	 * 申请接口
+	 */
+	apply: (payload) => {
+		return new Promise((resolve, reject) => {
+			axios
+				.post("/apply", payload)
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((e) => {
+					reject(e);
+				});
+		});
+	},
+	/**
+	 * 学院专业信息列表接口
+	 */
+	getSchoolList: () => {
+		return new Promise((resolve, reject) => {
+			axios
+				.get("/get-schoollist")
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((e) => {
+					reject(e);
+				});
+		});
+	}
+};
