08fe7512a2773fa7a4d6a04912d03e29702b77ad
Hokori23 <hokori23@qq.com>
2020-08-30 10:53:01 +0800

    Feat: Home页 Snackbar添加errno提示文本 && 多端UI适应 && 正则表达式验证

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,41 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from '@material-ui/core/styles';
 
 import { Home as HOME } from "../components";
 import api from "../api";
+import func from "../func";
+import { Portal } from "@material-ui/core";
 
-// import "../style/Home.scss";
 
-const useStyles = makeStyles((theme) => ({
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		boxSizing: 'border-box',
-		paddingTop: "10%",
-		// justifyContent: "center",
-		alignItems: "center",
-		height: "100%"
-	},
-	content: {
-		minHeight: 500,
-		"&__row": {
-			display: "flex"
-		}
-	},
-	textField: {
-		marginBottom: theme.spacing(1)
-	}
-}));
+
+
 
 // 解构component
-const { SchoolList, MajorList, TextField } = HOME;
+const { SchoolList, MajorList, TextField, Snackbar } = HOME;
 
 export default function Home() {
 	/**
 	 * Style
 	 */
-	const classes = useStyles();
+	const isSmallSize = useMediaQuery(useTheme().breakpoints.down('sm'));
+	const classes = makeStyles((theme) => ({
+		container: {
+			position: isSmallSize ? "static" : "absolute",
+			top:'50%',
+			left:'50%',
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+			width: isSmallSize ? '100%' : '80%',
+			height: isSmallSize ? "100%" : "auto",
+			padding: isSmallSize ? "10% 0" : "5% 0",
+			margin: 0,
+			transform: isSmallSize ? 'none' : 'translate(-50%, -52%)',
+			boxSizing: 'border-box',
+			overflow: "auto",
+			"& > .Home__content": {
+				width: "100%",
+				"& > .Home__content__body": {
+					display: isSmallSize ? "block" : "flex",
+					flexDirection: isSmallSize ? "column" : "row",
+					justifyContent: isSmallSize ? "normal" : "center",
+					marginTop: isSmallSize ? 0 : "30px",
+					"& > .Home__content__left, & > .Home__content__right": {
+						margin: isSmallSize ? 0 : "0 10%"
+					},
+					"& > .Home__content__right": {
+						display: isSmallSize ? "block" : "flex",
+						flexDirection: isSmallSize ? "row" : "column",
+						justifyContent: isSmallSize ? "normal" : "center"
+					},
+					"& .Home__content__row": {
+						display: "flex",
+						justifyContent: "center"
+					}
+				},
+			},
+			"& > .Home__footer" : {
+				marginTop: '5%',
+				backgroundColor: '#ffffff'
+			},
+		},
+		textField: {
+			marginBottom: theme.spacing(isSmallSize ? 1.5 : 3),
+			width: isSmallSize ? "60%" : 220,
+		}
+	}))();
+
+
+
+
 
 	/**
 	 * State
@@ -63,7 +102,11 @@ export default function Home() {
 		major_error: false,
 		student_num_error: false,
 		email_error: false,
-		qq_error: false
+		qq_error: false,
+		name_errno: 0,
+		student_num_errno: 0,
+		email_errno: 0,
+		qq_errno: 0,
 	});
 	// error 解构
 	const {
@@ -72,36 +115,56 @@ export default function Home() {
 		major_error,
 		student_num_error,
 		email_error,
-		qq_error
+		qq_error,
+		name_errno,
+		student_num_errno,
+		email_errno,
+		qq_errno,
 	} = error;
+
+	/** snackBar */
+	const [snackBar, setSnackBar] = useState({
+		code: 0,
+		message: '',
+		open: false,
+	})
+
+
+
+
+
 
 	/**
 	 * 用来遍历DOM的数组
 	 */
 	const textFieldList = [
-		// {
-		// 	label: "姓名*",
-		// 	value: name,
-		// 	name: "name",
-		// 	isError: { name_error }
-		// },
+		{
+			label: "姓名*",
+			value: name,
+			name: "name",
+			error: name_error,
+			errno: name_errno
+		},
 		{
 			label: "学号*",
 			value: student_num,
 			name: "student_num",
-			isError: { student_num_error }
+			error: student_num_error,
+			errno: student_num_errno
 		},
 		{
 			label: "邮箱*",
 			value: email,
 			name: "email",
-			isError: { email_error }
+			error: email_error,
+			errno: email_errno
 		},
 		{
 			label: "QQ*",
 			value: qq,
 			name: "qq",
-			isError: { qq_error }
+			error: qq_error,
+			errno: qq_errno
 		},
 		{
 			label: "简介",
@@ -110,26 +173,72 @@ export default function Home() {
 			multiline: true
 		},
 		{
-			label: "个人网站",
+			label: "个人博客 or 网站",
 			value: site,
 			name: "site"
 		}
-	].map((item) => {
-		const { label, name, value, multiline, isError } = item;
-		return (
+	].map((item, index) => {
+		const { label, name, value, multiline, error, errno } = item;
+		let node;
+		index === 1 ? node = (
+		<Fragment key={name}>
+			<div className="Home__content__row">
+				<SchoolList
+					data={schoolAndMajorList}
+					school={school}
+					onSchoolChange={handleSchoolChange}
+					error={school_error}
+					className={classes.textField}
+				/>
+			</div>
+			<div className="Home__content__row">
+				<MajorList
+					data={schoolAndMajorList}
+					school={school}
+					major={major}
+					onMajorChange={handleMajorChange}
+					error={major_error}
+					className={classes.textField}
+				/>
+			</div>
+			<div className="Home__content__row">
+				<TextField
+					label={label}
+					name={name}
+					value={value}
+					error={error}
+					errno={errno}
+					multiline={multiline}
+					onTextFieldChange={handleTextFieldChange}
+					onTextFieldBlur={handleTextFieldValidate}
+				></TextField>
+			</div>
+		</Fragment>
+		) :
+		node = (
 			<div className="Home__content__row" key={name}>
 				<TextField
 					label={label}
 					name={name}
 					value={value}
-					isError={isError}
+					error={error}
+					errno={errno}
 					multiline={multiline}
-					className={classes.textField}
 					onTextFieldChange={handleTextFieldChange}
+					onTextFieldBlur={handleTextFieldValidate}
 				></TextField>
 			</div>
 		);
+		return node;
 	});
+
+	const contentRight = textFieldList.splice(4)
+	const contentLeft = textFieldList
+
+
+
+
+
 
 	/**
 	 * Function
@@ -157,26 +266,100 @@ export default function Home() {
 		}));
 	}
 	function handleTextFieldChange(val, name, isRequired) {
-		isRequired &&
-			setError((state) => ({
-				...state,
-				[`${name}_error`]: !val
-			}));
+		isRequired && setError((state) => ({
+			...state,
+			[`${name}_error`]: !val
+		}));
+		
 		setNewApply((state) => ({
 			...state,
 			[name]: val
 		}));
 	}
+	function handleTextFieldValidate(val, name, isRequired) {
+		const REG_RULES = {
+			name: /^[\u4e00-\u9fa5]{2,10}$/,
+			student_num: /^\d{10}$/,
+			qq: /^\d{5,}$/,
+			email: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/,
+		}
+		// 检查必填字段是否为空
+		let error = isRequired ? !val : false;
+		// 已填字段是否通过验证规则
+		const errno = val && func.isDef(REG_RULES[name]) && !REG_RULES[name].test(val) ? 1 : 0;
+		error = errno === 1 ? true : error;
+		setError((state) => ({
+			...state,
+			[`${name}_errno`]: errno,
+			[`${name}_error`]: error
+		}));
+	}
+	function isValidate() {
+		let flag = true;
+		// 检查必填字段是否为空
+		for(let i in newApply) {
+			const errorState = error[`${i}_error`];
+			if (!newApply[i] && func.isDef(errorState)) {
+				flag = false;
+				setError((state)=> ({
+					...state,
+					[`${i}_error`]: true
+				}))
+			}
+		}
+		!flag && setSnackBar((state) => ({
+			code: 1,
+			message: '请正确填写表单必填字段',
+			open: true
+		}))
+		return flag;
+	}
+	function handleSnackBarClose() {
+		setSnackBar((state)=>({
+			...state,
+			open: false,
+		}))
+	}
+	async function handleApply() {
+		if (!isValidate())  return;
+		try {
+			const payload = JSON.parse(JSON.stringify(newApply));
+			payload.site = `https://${payload.site}/`;
+			const { code, message } = await api.apply(payload);
+			const newSnackBar = {
+				open: true,
+				message: message
+			}
+			code ? newSnackBar.code = 1 : newSnackBar.code = 0;
+			setSnackBar(()=>newSnackBar);
+		} catch (e) {
+			setSnackBar(()=>({
+				code: 2,
+				open: true,
+				message: e.message
+			}))
+		}
+	}
 	async function getData() {
-		console.log("getData");
 		try {
 			const { code, data } = await api.getSchoolList();
 			if (code) {
 				return;
 			}
 			data && setSchoolAndMajorList(data);
-		} catch (e) {}
+		} catch (e) {
+			setSnackBar(()=>({
+				code: 2,
+				open: true,
+				message: e.message
+			}))
+		}
 	}
+
+
+
+
+
 
 	/**
 	 * Hook
@@ -187,37 +370,44 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className={classes.container}>
-			<div className={classes.content}>
-				<div className="Home__content__row">
-					<TextField
-						label="姓名*"
-						value={name}
-						name="name"
-						isError={name_error}
-						className={classes.textField}
-						onTextFieldChange={handleTextFieldChange}
-					/>
+		<Paper className={classes.container} elevation={9}>
+			<div className="Home__content">
+				<Typography variant={isSmallSize ? "subtitle1" : "h6"} component="header" color="primary" gutterBottom style={
+					{
+						userSelect: 'none',
+						textAlign: 'center'
+					}
+				}>
+					Wego 前端社团申请
+				</Typography>
+				<Typography variant={"caption"} component="div" color="textPrimary" style={
+					{
+						userSelect: 'none',
+						textAlign: 'center'
+					}
+				}>
+					在这下面提交你的申请哦
+				</Typography>
+				<div className="Home__content__body">
+					<div className="Home__content__left">
+						{contentLeft}
+					</div>
+					<div className="Home__content__right">
+						{contentRight}
+					</div>
 				</div>
-				<div className="Home__content__row">
-					<SchoolList
-						data={schoolAndMajorList}
-						school={school}
-						onSchoolChange={handleSchoolChange}
-						isError={school_error}
-					/>
-				</div>
-				<div className="Home__content__row">
-					<MajorList
-						data={schoolAndMajorList}
-						school={school}
-						major={major}
-						onMajorChange={handleMajorChange}
-						isError={major_error}
-					/>
-				</div>
-				{textFieldList}
 			</div>
-		</div>
+			<div className="Home__footer">
+				<Button variant="outlined" color="primary" onClick={handleApply}>提交申请</Button>
+			</div>
+			<Portal>
+				<Snackbar
+					code={snackBar.code}
+					message={snackBar.message}
+					open={snackBar.open}
+					handleClose={handleSnackBarClose}
+				></Snackbar>
+			</Portal>
+		</Paper>
 	);
 }
